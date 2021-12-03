1ec22463a0e15c4f496433c65403d10a32fcc620
deksa89 <89583742+deksa89@users.noreply.github.com>
2021-12-03 15:14:51 +0100

    Add files via upload

---
+++ zadaci 171121/zadaci_pandas0312.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+data = pd.read_csv("letovi.csv", index_col=0)
+#print(data.head())
+
+
+#1. Loadaj Deanovu tablicu (pazi na header)
+#print(data.shape)
+
+#print(data.size)
+
+#print(data.describe())
+
+
+#2. Prikaži podatke o tablici
+    # (koje metode ćete koristiti uvijek za dataframeove o kojima ne znate ništa?)
+
+data.dropna(inplace=True)
+
+# new data frame with split value columns
+new = data["Prices"].str.split(" ", n=1, expand=True)
+
+# making separate first name column from new data frame
+data["value"] = new[0]
+
+# making separate last name column from new data frame
+data["currency"] = new[1]
+
+# Dropping old Name columns
+data.drop(columns=["Prices"], inplace=True)
+
+# df display
+print(data.describe())
+
+
+for i, row in data.iterrows():
+    euri = round((float(row["value"].replace(".", "")) / 7.4),2)
+    #print(euri)
+
+    data.at[i, "value"] = euri
+    data.at[i, "currency"] = "EUR"
+
+
+
+print(data.head())
